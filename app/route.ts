d8a3affa3ce43fae32d92bf904cac8da201744a0
8cawaii-pixel <8cawaii@gmail.com>
2026-03-12 09:14:50 +0000

    first

---
+++ app/route.ts
@@ -0,0 +1,28 @@
+// app/route.js
+export const runtime = 'edge';
+
+export async function GET(request: { url: string | URL; }) {
+    const { searchParams } = new URL(request.url);
+    const imageUrl = searchParams.get('url');
+
+    if (!imageUrl) {
+        return new Response('Missing "url" parameter', { status: 400 });
+    }
+
+    try {
+        const response = await fetch(imageUrl);
+
+        // Create a streaming response with the correct headers
+        return new Response(response.body, {
+            status: response.status,
+            headers: {
+                'Access-Control-Allow-Origin': 'https://bisa.work', // Replace with your domain
+                'Content-Type': response.headers.get('Content-Type') || 'application/octet-stream',
+                'Content-Disposition': 'attachment',
+                'Cache-Control': 'public, max-age=31536000, immutable', // Cache heavily to save transit
+            },
+        });
+    } catch (error) {
+        return new Response('Proxy Error', { status: 500 });
+    }
+}
